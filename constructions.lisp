cb9f233b3ba9db3154230f6f517047765264fdcc
Robert Smith <quad@symbo1ics.com>
2013-03-08 01:16:18 -0800

    make GLUE n-ary, add some fox

--- constructions.lisp
+++ constructions.lisp
@@ -1,12 +1,14 @@
 ;;;; constructions.lisp
 ;;;; Copyright (c) 2013 Robert Smith
 
-(defun glue (a b)
-  (row-box (list a b)))
+(defun glue (&rest items)
+  "Glue together (horizontally) all of ITEMS."
+  (row-box items))
 
 (defvar +sigma+
   (picture-box '("==="
                  "\\"
                  " >"
                  "/"
-                 "===")))
+                 "==="))
+  "The capital sigma symbol used for summation.")
