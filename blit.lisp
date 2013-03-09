5d035917d367c56112c50275ee3c2b97621e9705
Robert Smith <quad@symbo1ics.com>
2013-03-09 03:21:24 -0800

    more tabs

--- blit.lisp
+++ blit.lisp
@@ -136,16 +136,16 @@
 
 (defmethod blit (canvas (box parens-box) x y)
   (let* ((contents (parens-box-contents box))
-	 (h (height contents)))
+         (h (height contents)))
     (if (= h 1)
-	(blit canvas (glue #\( contents #\)) x y)
-	(progn
-	  (blit canvas contents (+ x 2) y)
-	  (blit canvas #\/  x y)
-	  (blit canvas #\\ (+ x (width contents) 3) y)
-	  (loop :for i :from 1 :below h
-		:do (progn
-		      (blit canvas #\| x (+ y i))
-		      (blit canvas #\| (+ x (width contents) 3) (+ y i))))
-	  (blit canvas #\\ x (+ y (- h 1)))
-	  (blit canvas #\/  (+ x (width contents) 3) (+ y (- h 1)))))))
+        (blit canvas (glue #\( contents #\)) x y)
+        (progn
+          (blit canvas contents (+ x 2) y)
+          (blit canvas #\/  x y)
+          (blit canvas #\\ (+ x (width contents) 3) y)
+          (loop :for i :from 1 :below h
+                :do (progn
+                      (blit canvas #\| x (+ y i))
+                      (blit canvas #\| (+ x (width contents) 3) (+ y i))))
+          (blit canvas #\\ x (+ y (- h 1)))
+          (blit canvas #\/  (+ x (width contents) 3) (+ y (- h 1)))))))
