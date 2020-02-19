5648fda4dd5772813534821cb995c85c50bcd5a3
Serialdev <carlos.mariscal.melgar@gmail.com>
2020-02-19 08:20:25 -0800

    [+] Added usage example & elisp provide

--- formulador.el
+++ formulador.el
@@ -1,3 +1,29 @@
+;;; formulador.el --- Allow usage of formulador from within emacs   -*- lexical-binding: t -*-
+
+;; Copyright (C) 2019-  Andres Mariscal
+
+;; Author: Andres Mariscal <carlos.mariscal.melgar@gmail.com>
+
+;; URL: https://github.com/serialdev/tiqsi-emacs
+;; Keywords: lisp
+;; Version: 0
+;; Package-Requires: ((cl-lib "0.5") (emacs "24"))
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+;;; Commentary:
 
 (require 's)
 (require 'pos-tip)
@@ -13,7 +39,7 @@
 
 (defmacro formulador--ros-commands (&rest commands)
   `(shell-command-to-string
-   (concat 
+   (concat
     (concat "ros --eval '" ,@commands)
     "'")))
 
@@ -31,15 +57,21 @@
      (kill-new command)
   ))
 
-  
 
 
-(formulador-command
+; ------------------------------------------------------------------------- ;
+;                               Usage Example                               ;
+; ------------------------------------------------------------------------- ;
 
- "
-(defun pow (a b)
-  (formulador:script-box a :superscript b))
+;; (formulador-command
 
-(print (formulador:draw (pow (formulador:box \" | \") (formulador:box \"r\"))))"
- )
+;;  "
+;; (defun pow (a b)
+;;   (formulador:script-box a :superscript b))
 
+;; (print (formulador:draw (pow (formulador:box \" | \") (formulador:box \"r\"))))"
+;;  )
+
+(provide 'formulador)
+
+;;; formulador.el ends here
